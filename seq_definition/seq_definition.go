61549b66962f2f03504321aba176ce49ebada842
mattbro2 <mattbro2@cisco.com>
2016-08-24 16:48:16 -0500

    Updated regex for windows paths

--- seq_definition/seq_definition.go
+++ seq_definition/seq_definition.go
@@ -1,7 +1,7 @@
 //Package seq_definition is where the regexes for file sequences are defined, you may edit this file if needed
 //Original regexes are:
-//Reducer: ".*[\.\_\ \/]([0-9]+)\.\w{2,4}$"
-//Expander: ".*[\.\_\ \/](\[[0-9-,]+\])\."
+//Reducer: ".*[\.\_\ \/\\]([0-9]+)\.\w{2,4}$"
+//Expander: ".*[\.\_\ \/\\](\[[0-9-,]+\])\."
 //Note that there is a group inside the regex around the number component of the file or sequence listing.
 package seq_definition
 
@@ -12,8 +12,8 @@ type Seq_definition struct {
 
 func SeqDefinition() (Seq_definition, error) {
 	seq_def := Seq_definition{
-		ReducerRegex:  `.*[\.\_\ \/]([0-9]+)\.\w{2,4}$`,
-		ExpanderRegex: `.*[\.\_\ \/](\[[0-9-,]+\])\.`,
+		ReducerRegex:  `.*[\.\_\ \/\\]([0-9]+)\.\w{2,4}$`,
+		ExpanderRegex: `.*[\.\_\ \/\\](\[[0-9-,]+\])\.`,
 	}
 	return seq_def, nil
 }
